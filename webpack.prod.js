56b7a3c8e6f5b882b2d2672ba8ac1a0f93747740
bitroy <pranoyedu@gmail.com>
2025-08-16 15:57:31 +0530

    fix: hybrid setup for remote apps

--- webpack.prod.js
+++ webpack.prod.js
@@ -18,7 +18,7 @@ module.exports = merge(common, {
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "[name].[contenthash].js",
-		publicPath: "/todo/",
+		publicPath: "/",
 	},
 	plugins: [
 		new ModuleFederationPlugin({
